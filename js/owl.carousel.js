4f436858f2ed05af9fa004fb768009891db771c5
KhalidM53 <57159395+KhalidM53@users.noreply.github.com>
2020-01-13 20:49:49 -0500

    js

--- js/owl.carousel.js
+++ js/owl.carousel.js
@@ -3,16 +3,7 @@
  
  */
 /**
- * Owl carousel
- * @version 2.3.3
- * @author Bartosz Wojciechowski
- * @author David Deutsch
- * @license The MIT License (MIT)
- * @todo Lazy Load Icon
- * @todo prevent animationend bubling
- * @todo itemsScaleUp
- * @todo Test Zepto
- * @todo stagePadding calculate wrong active classes
+ * 
  */
 ;(function($, window, document, undefined) {
 
